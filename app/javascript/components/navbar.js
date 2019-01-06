b841e7c6ccb9fd0a72599471d88e8a58198cd0ad
Lynn De Souza <lynn.desouza@alumni.uct.ac.za>
2019-01-06 15:27:38 +0000

    fixes after comments

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -5,7 +5,7 @@ function fixNavbarOnScroll() {
   const sticky = navbar.offsetTop;
 
   window.addEventListener("scroll", (event) => {
-    if (window.pageYOffset >= 1200) {
+    if (window.pageYOffset >= 1000) {
         navbar.classList.add('sticky');
       } else {
         navbar.classList.remove('sticky');
