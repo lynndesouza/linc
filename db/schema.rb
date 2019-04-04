3dd5044a7a447f507e5abd5b8256c918913feb35
Lynn De Souza <lynn.desouza@alumni.uct.ac.za>
2019-04-04 19:23:54 +0100

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_174405) do
+ActiveRecord::Schema.define(version: 2019_03_19_153449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2019_03_19_174405) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
   end
 
 end
